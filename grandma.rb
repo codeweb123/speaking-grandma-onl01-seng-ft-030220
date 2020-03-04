d0ed1fe1e86c4dd4c18f888b9e54caed2c512c23
interactive-sequence-2417 <jiyonj@hotmail.com>
2020-03-04 19:53:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
-    "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   else
-    "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
 end
 
 
